7432629fde20e7113f99e6aa4917b2ac50937311
abelureste <aureste03@gmail.com>
2025-07-30 23:57:22 -0400

    Updated home link

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import BarWebsiteProject from '../components/BarWebsiteProject';
 
 function App() {
   return (
-    <Router basename="/abelureste.github.io/">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/personal-introduction" element={<PersonalIntroduction />} />
